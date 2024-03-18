c561fab2784e7388d8433cca3bed8e57b4ededb4
gamesferabr <gabriel@timepix.com.br>
2024-03-18 13:24:30 -0700

    modified:   build.sh

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ export GOOGLE_APPLICATION_CREDENTIALS="/mycredentials.json"
 export DB_NAME="postgres_new"
 export DB_USER="postgres_new"
 export DB_PASSWORD="PKp7nYu#_%~}1K2I"
-export DB_CONNECTION_NAME="seu_id_do_projeto:regiao:nome_da_instancia"
+export DB_CONNECTION_NAME="brave-set-409318:us-central1:myowndb"
 export DJANGO_SETTINGS_MODULE=gym_api.settings
 
 # Inicia o Cloud SQL Proxy em background
