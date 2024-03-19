bd33390c5527d1db7848f6a66912f0e18e4a6869
gamesferabr <gabriel@timepix.com.br>
2024-03-19 06:36:55 -0700

    modified:   build.sh

--- build.sh
+++ build.sh
@@ -4,18 +4,18 @@
 set -e
 
 # Define variáveis de ambiente (ajuste conforme necessário)
-export GOOGLE_APPLICATION_CREDENTIALS="/mycredentials.json"
+export GOOGLE_APPLICATION_CREDENTIALS="mycredentials.json"
 export DB_NAME="postgres_new"
 export DB_USER="postgres_new"
 export DB_PASSWORD="PKp7nYu#_%~}1K2I"
 export DB_CONNECTION_NAME="brave-set-409318:us-central1:myowndb"
-export DJANGO_SETTINGS_MODULE=gym_api.settings
+export DJANGO_SETTINGS_MODULE= gym_api.settings
 
 # Inicia o Cloud SQL Proxy em background
-./cloud_sql_proxy -instances=$DB_CONNECTION_NAME=tcp:5432 &
+./gym_api/cloud-sql-proxy -instances=$DB_CONNECTION_NAME=tcp:5432 &
 
 # Ativa o ambiente virtual
-source venv/bin/activate
+source ./venv/Scripts/activate
 
 # Instala dependências
 pip install -r requirements.txt
