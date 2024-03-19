0a49989cad0284afee0e1f164350d32e8071c675
gamesferabr <gabriel@timepix.com.br>
2024-03-19 07:39:07 -0700

    modified:   build.sh 	modified:   gym_api/gym_api/urls.py

--- build.sh
+++ build.sh
@@ -11,8 +11,7 @@ export DB_PASSWORD="PKp7nYu#_%~}1K2I"
 export DB_CONNECTION_NAME="brave-set-409318:us-central1:myowndb"
 export DJANGO_SETTINGS_MODULE="gym_api.gym_api.settings"
 
-# Inicia o Cloud SQL Proxy em background
-./gym_api/cloud-sql-proxy.exe -instances=$DB_CONNECTION_NAME=tcp:5432 &
+
 
 # Ativa o ambiente virtual
 source ./venv/Scripts/activate
@@ -22,6 +21,9 @@ pip install -r requirements.txt
 
 # Inicia o servidor Django (ajuste conforme necessário, por exemplo, usando gunicorn)
 gunicorn gym_api.gym_api.wsgi:application
+
+# Inicia o Cloud SQL Proxy em background
+./gym_api/cloud-sql-proxy $DB_CONNECTION_NAME
 
 # Para encerrar o Cloud SQL Proxy quando o script terminar
 trap "kill $!" EXIT
