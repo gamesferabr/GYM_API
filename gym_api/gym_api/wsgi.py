afaa5b4106aa742c8d4ecfbcaf4badd5e9d139a1
gamesferabr <gabriel@timepix.com.br>
2024-03-19 13:11:08 -0700

    modified:   gym_api/gym_api/wsgi.py

--- gym_api/gym_api/wsgi.py
+++ gym_api/gym_api/wsgi.py
@@ -8,9 +8,15 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
 import os
-
+import sys
 from django.core.wsgi import get_wsgi_application
+
+# Adiciona o diretório do projeto ao sys.path
+proj_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+if proj_path not in sys.path:
+    sys.path.append(proj_path)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gym_api.settings')
 
 application = get_wsgi_application()
+
