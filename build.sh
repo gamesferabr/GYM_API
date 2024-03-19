504ffe3e82979b7675a89e6060965eb8c6c2905b
gamesferabr <gabriel@timepix.com.br>
2024-03-19 09:06:45 -0700

    modified:   build.sh

--- build.sh
+++ build.sh
@@ -14,7 +14,7 @@ export DJANGO_SETTINGS_MODULE="gym_api.gym_api.settings"
 
 # Inicia o Cloud SQL Proxy em background
 # Torna o Cloud SQL Proxy executável
-chmod +x ./gym_api/cloud_sql_proxy
+chmod +x ./gym_api/cloud_sql_proxy.linux.amd64
 
 # Inicia o Cloud SQL Proxy em background
 ./gym_api/cloud_sql_proxy ${DB_CONNECTION_NAME} &
