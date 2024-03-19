163c1cb4dd348808bc384c6a15830b620390314e
gamesferabr <gabriel@timepix.com.br>
2024-03-19 06:50:28 -0700

    modified:   build.sh

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ export DB_NAME="postgres_new"
 export DB_USER="postgres_new"
 export DB_PASSWORD="PKp7nYu#_%~}1K2I"
 export DB_CONNECTION_NAME="brave-set-409318:us-central1:myowndb"
-export DJANGO_SETTINGS_MODULE= ./gym_api/gym_api/settings.py
+export DJANGO_SETTINGS_MODULE="gym_api.gym_api.settings"
 
 # Inicia o Cloud SQL Proxy em background
 ./gym_api/cloud-sql-proxy.exe -instances=$DB_CONNECTION_NAME=tcp:5432 &
