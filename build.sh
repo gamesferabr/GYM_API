ed8b6574f25f55aed98015a9ef0c722beb29e4e9
gamesferabr <gabriel@timepix.com.br>
2024-03-19 12:34:47 -0700

    modified:   build.sh 	renamed:    manage.py -> gym_api/manage.py

--- build.sh
+++ build.sh
@@ -7,5 +7,5 @@ source venv/Scripts/activate
 
 pip install -r requirements.txt
 
-python manage.py collectstatic --no-input
-python manage.py migrate
+python gym_api/manage.py collectstatic --no-input
+python gym_api/manage.py migrate
