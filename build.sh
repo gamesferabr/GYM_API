6775c9c2979f3420260b6541ff4b335ad06ca1f6
gamesferabr <gabriel@timepix.com.br>
2024-03-19 06:45:02 -0700

    modified:   build.sh

--- build.sh
+++ build.sh
@@ -9,9 +9,10 @@ export DB_NAME="postgres_new"
 export DB_USER="postgres_new"
 export DB_PASSWORD="PKp7nYu#_%~}1K2I"
 export DB_CONNECTION_NAME="brave-set-409318:us-central1:myowndb"
+export DJANGO_SETTINGS_MODULE= ./gym_api/gym_api.settings
 
 # Inicia o Cloud SQL Proxy em background
-./gym_api/cloud-sql-proxy -instances=$DB_CONNECTION_NAME=tcp:5432 &
+./gym_api/cloud-sql-proxy.exe -instances=$DB_CONNECTION_NAME=tcp:5432 &
 
 # Ativa o ambiente virtual
 source ./venv/Scripts/activate
@@ -19,14 +20,8 @@ source ./venv/Scripts/activate
 # Instala dependências
 pip install -r requirements.txt
 
-# Aplica as migrações do Django
-python manage.py migrate
-
-# Coleta os arquivos estáticos
-python manage.py collectstatic --noinput
-
 # Inicia o servidor Django (ajuste conforme necessário, por exemplo, usando gunicorn)
-python manage.py runserver
+gunicorn gym_api.gym_api.wsgi:application
 
 # Para encerrar o Cloud SQL Proxy quando o script terminar
 trap "kill $!" EXIT
